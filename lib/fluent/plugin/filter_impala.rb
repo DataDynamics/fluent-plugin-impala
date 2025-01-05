791aaf810c1b69ba64d8bfce0002598c6c9a3e58
KIM BYOUNG GON <fharenheit@gmail.com>
2025-01-05 15:39:49 +0900

    테스트 코드 추가

--- lib/fluent/plugin/filter_impala.rb
+++ lib/fluent/plugin/filter_impala.rb
@@ -14,6 +14,46 @@ module Fluent
         print "FILTER STARTED"
         record
       end
+
+      def kudu_backpressure(message)
+        result = {}
+        matched = message.include?("The service queue is full")
+        if matched
+          result.merge!(kudu_thread_pool_task_running(message))
+          result.merge!(kudu_thread_pool_task_queued(message))
+          result.merge!(kudu_thread_pool_item(message))
+          result
+        end
+        result
+      end
+
+      def kudu_thread_pool_task_running(message)
+        result = {}
+        start_index = message.index("(") + 1
+        end_index = message.index("tasks running") - 1
+        current_tasks, max_tasks = message[start_index..end_index].strip.split("/").map(&:to_i)
+        result.store("current_running_tasks", current_tasks)
+        result.store("max_running_tasks", max_tasks)
+        result
+      end
+
+      def kudu_thread_pool_task_queued(message)
+        result = {}
+        start_index = message.index("tasks running , ") + 1
+        end_index = message.index("tasks queued") - 1
+        current_tasks, max_tasks = message[start_index..end_index].strip.split("/").map(&:to_i)
+        result.store("current_queued_tasks", current_tasks)
+        result.store("max_queued_tasks", max_tasks)
+        result
+      end
+
+      def kudu_thread_pool_item(message)
+        result = {}
+        start_index = message.index("The service queue is full; it has ") + 1
+        end_index = message.index("items") - 1
+        result.store("service_queue_count", message[start_index..end_index].strip)
+        result
+      end
     end
   end
 end
