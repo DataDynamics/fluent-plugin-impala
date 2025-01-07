a9860c3c9c6db66d24fa24ca378e00a866e7a04a
김병곤 <fharenheit@gmail.com>
2025-01-08 08:22:02 +0900

    설정 정보 추가

--- lib/fluent/plugin/impala.rb
+++ lib/fluent/plugin/impala.rb
@@ -54,29 +54,3 @@ module Fluent
     end
   end
 end
-
-# test "message1_test" do
-#   assert_true true, messages1[0][:message].include?("stmt=")
-#
-#   r = filter(CONFIG, messages1)
-#
-#   assert_equal "QUERY", r[0]["type"]
-#   assert_equal "SELECT `t`.`company`, `t`.`gbm`, `t`.`createdate`, `t`.`datatype`, `t`.`isvalid`, `t`.`sono`, `t`.`soitem`, `t`.`sosche`, `t`.`documentno`, `t`.`documentitem`, `t`.`atpdoc`, `t`.`dmdship`, `t`.`dmdlnitem` FROM `kududb`.`dyn_salesorder_dp` `t` LIMIT 501", r[0]["table_name"]
-# end
-#
-# test "message2_test" do
-#   assert_true true, messages2[0][:message].include?("Invalid or unknown query handle")
-#
-#   r = filter(CONFIG, messages2)
-#
-#   assert_equal "INVALID_HANDLE", r[0]["type"]
-#   assert_equal "5944906400e507af:990bc41b00000000", r[0]["query_id"]
-# end
-#
-# test "message3_test" do
-#   assert_true true, messages3[0][:message].include?("THRIFT_EAGAIN (timed out)")
-#
-#   r = filter(CONFIG, messages3)
-#
-#   assert_equal "THRIFT_TIMEOUT", r[0]["type"]
-# end
--- package.sh
+++ package.sh
@@ -3,5 +3,4 @@
 systemctl stop fluentd
 /opt/fluent/bin/gem build fluent-plugin-impala.gemspec
 /opt/fluent/bin/gem install fluent-plugin-impala-0.1.0.gem
-/opt/fluent/bin/gem update fluent-plugin-impala-0.1.0.gem
 ls -lsa /opt/fluent/lib/ruby/gems/3.2.0/gems/fluent-plugin-impala-0.1.0
