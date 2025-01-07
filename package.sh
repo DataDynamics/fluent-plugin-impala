d40ed4ccbccce8e88e02cd79c3d6085aaa1147c9
김병곤 <fharenheit@gmail.com>
2025-01-08 08:38:20 +0900

    설정 정보 추가

--- package.sh
+++ package.sh
@@ -8,3 +8,5 @@ ls -lsa /opt/fluent/lib/ruby/gems/3.2.0/gems/fluent-plugin-impala-0.1.0
 rm -rf /var/log/fluent/*.log
 rm -rf /var/log/fluent/*.pos
 systemctl start fluentd
+
+tail -1000f /var/log/fluent/fluentd.log
