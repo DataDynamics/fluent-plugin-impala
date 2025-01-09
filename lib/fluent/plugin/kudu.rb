e34df76eee3f54bb921a04f4c7fe0672ad082739
김병곤 <fharenheit@gmail.com>
2025-01-09 10:44:33 +0900

    로그 탐색 조건 추가

--- lib/fluent/plugin/kudu.rb
+++ lib/fluent/plugin/kudu.rb
@@ -12,6 +12,9 @@ module Fluent
       end
 
       def run
+
+        # W0827 10:27:11.563266 18970 leader_election.cc:287] T 5e52666b9ecf4460808b387a7bfe67d6 P e8a1d4cacb7f4f219fd250704004d258 [CANDIDATE]: Term 1968 pre-election: RPC error from VoteRequest() call to peer 3a5ee46ab1284f4e9d4cdfe5d0b7f7fa: Remote error: Service unavailable: RequestConsensusVote request on kudu.consensus.ConsensusService from 10.250.250.19:37042 dropped due to backpressure. The service queue is full; it has 50 items.
+
         if record["message"].include?("The service queue is full")
           @record.store("category", "prometheus")
           @record.store("type", "BACKPRESSURE")
@@ -28,6 +31,7 @@ module Fluent
           @record
         end
 
+
         if record["message"].include?("exceeded configure scan timeout")
           @record.store("category", "prometheus")
           @record.store("type", "SCAN_TIMEOUT")
@@ -41,11 +45,50 @@ module Fluent
           @record
         end
 
+        # Timed out: Failed to write batch of 227 ops to tablet 8b19e4a0362e4b82941e54d33ac9c5a2 after 1 attempt(s): Failed to write to server: b2ead65ab0164f5b8db24d700a2c474a (wewcw0hd3dn02.example.com:7050): Write RPC to 10.11.100.85:7050 timed out after 179.977s (SENT)
+
         if record["message"].include?("Failed to write batch")
           @record.store("category", "prometheus")
           @record.store("type", "WRITER_TIMEOUT")
           @record.store("metric-type", "Counter")
           @record.store("metric-name", "kudu_write_batch_timeout_count")
+          @record.store("job", "fluentd-plugin-kudu")
+          @record.store("instance", @hostname)
+          @record
+        end
+
+        # W0922 00:56:52.313848 10858 inbound_call.cc:193] Call kudu.consensus.ConsensusService.UpdateConsensus
+        # from 192.168.1.102:43499 (request call id 3555909) took 1464ms (client timeout 1000).
+
+        if record["message"].include?("client timeout")
+          @record.store("category", "prometheus")
+          @record.store("type", "CLIENT_TIMEOUT")
+          @record.store("metric-type", "Counter")
+          @record.store("metric-name", "kudu_client_timeout_count")
+          @record.store("job", "fluentd-plugin-kudu")
+          @record.store("instance", @hostname)
+          @record
+        end
+
+        # Service unavailable: Soft memory limit exceeded (at 96.35% of capacity)
+
+        if record["message"].include?("Soft memory limit exceeded")
+          @record.store("category", "prometheus")
+          @record.store("type", "MEMORY_LIMIT_EXCEEDED")
+          @record.store("metric-type", "Counter")
+          @record.store("metric-name", "kudu_soft_memory_limit_exceeded_count")
+          @record.store("job", "fluentd-plugin-kudu")
+          @record.store("instance", @hostname)
+          @record
+        end
+
+        # W0926 11:19:01.339553 27231 net_util.cc:129] Time spent resolving address for kudu-tserver.example.com: real 4.647s    user 0.000s     sys 0.000s
+
+        if record["message"].include?("Time spent resolving address")
+          @record.store("category", "prometheus")
+          @record.store("type", "DNS_RESOLVE_SLOW")
+          @record.store("metric-type", "Counter")
+          @record.store("metric-name", "kudu_dns_resolve_slow_count")
           @record.store("job", "fluentd-plugin-kudu")
           @record.store("instance", @hostname)
           @record
